beaf8587624563fa1f8b1ab5a6ee990bd3a7d688
muralivnv <muralivnv@gmail.com>
2021-09-18 23:12:55 -0400

    Using thread to parse messages and refactoring

--- include/cppyplot_server.py
+++ include/cppyplot_server.py
@@ -1,36 +1,4 @@
-import zmq
-import sys
-
-from threading import Thread
-import struct
-
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-if (len(sys.argv) > 1):
-    socket.connect(sys.argv[1])
-else:
-    socket.connect("tcp://127.0.0.1:5555")
-socket.setsockopt(zmq.LINGER, 0)
-socket.setsockopt_string(zmq.SUBSCRIBE, "")
-
-############
-## function to receive all publisher messages
-############
-import queue
-msg_queue   = queue.Queue()
-kill_thread = False
-
-def subscriber():
-    global socket, msg_queue
-    while (not kill_thread):
-        if (socket.poll(50, zmq.POLLIN)):
-            zmq_message = socket.recv()
-            msg_queue.put(zmq_message)
-
-subscriber_thread = Thread(target=subscriber)
-subscriber_thread.start()
-print("[INFO] Started subscriber thread ")
-
+### Custom Imports ###
 lib_sym = {}
 
 ## Import numpy and register the object in the symbol table
@@ -39,7 +7,9 @@ lib_sym['np'] = np
 
 ## Import matplotlib and register plot object in the symbol table
 import matplotlib.pyplot as plt
+import matplotlib.cm  as cm
 lib_sym['plt'] = plt
+lib_sym['cm'] = cm
 
 # from matplotlib.animation import FuncAnimation
 # lib_sym['FuncAnimation'] = FuncAnimation
@@ -63,21 +33,62 @@ lib_sym['plt'] = plt
 # lib_sym['output_file']      = output_file
 # lib_sym['show']             = show
 
+# Import plotly
+# import plotly.io as pio
+# # pio.renderers.default = "browser" # comment this when using Dash for rendering
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
+# lib_sym['go']            = go
+# lib_sym['make_subplots'] = make_subplots
+# lib_sym['pio']           = pio
 
+# Import dash
+# import dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# lib_sym['dash'] = dash
+# lib_sym['dcc'] = dcc
+# lib_sym['html'] = html
+
+#### required imports ####
+import zmq
+import sys
+from threading import Thread
+from struct import unpack
+from queue import Queue
 from asteval import Interpreter, make_symbol_table
-aeval          = Interpreter()
-aeval.symtable = make_symbol_table(use_numpy=True, **lib_sym)
-plot_cmd  = None
-plot_data = {}
 
+#### Globals #####
+recv_msgs   = Queue()
+parsed_msgs = Queue()
+kill_thread = False
+
+aeval = Interpreter()
+aeval.symtable = make_symbol_table(use_numpy=True, **lib_sym, no_print=False)
+
+# indices for data access
 SYM_IDX   = 1
 TYPE_IDX  = 2
 LEN_IDX   = 3
 SHAPE_IDX = 4
 
-print("[INFO] Plotting server initialized ...")
+#### utility functions ####
+def subscriber(addr):
+    global recv_msgs
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect(addr)
+    socket.setsockopt(zmq.LINGER, 0)
+    socket.setsockopt_string(zmq.SUBSCRIBE, "")
+    
+    print("[INFO] started subscriber thread ")
+    print(f"[INFO] listening to {addr}")
+    while (not kill_thread):
+        if (socket.poll(50, zmq.POLLIN)):
+            zmq_message = socket.recv()
+            recv_msgs.put(zmq_message)
 
-def parse_shape(shape_str:str)->tuple:
+def parse_shape(shape_str:str)->list:
     shape = []
     axis_size = ""
     for char in shape_str:
@@ -87,60 +98,108 @@ def parse_shape(shape_str:str)->tuple:
             if (axis_size != ""):
                 shape.append(int(axis_size))
                 axis_size = ""
-    
-    return tuple(shape)
+    return shape
 
-def handle_payload(data, data_type, data_len, data_shape):
+def handle_payload(data, data_type, data_len, data_shape, _unpack=unpack):
     if ((data_type == 'c') or (data_type == 'b') or (data_type == 'B')):
-        data_converted = struct.unpack("="+(data_type*data_len), data)
+        data_converted = _unpack("="+(data_type*data_len), data)
         return (b''.join(data_converted)).decode("utf-8")
     else:
         if data_shape[0] > 0:
             return np.ndarray(data_shape, dtype="="+data_type, buffer=data)
         else:
-            return (struct.unpack("="+data_type, data))[0]
+            return (_unpack("="+data_type, data))[0]
 
-try:
-    while(True):
+def update_data(header, data, plot_data:dict)->dict:
+    data_info     = header.decode("utf-8").split('|')
+
+    # 0: data, 1: var_name, 2: var_type, 3: n_elems, 4: array_shape
+    data_type     = data_info[TYPE_IDX]
+    data_len      = int(data_info[LEN_IDX])
+    data_shape    = parse_shape(data_info[SHAPE_IDX])
+    plot_data[data_info[SYM_IDX]] = handle_payload(data, data_type, data_len, data_shape)
+
+    return plot_data
+
+def update_cmd(zmq_message)->str:
+    return zmq_message.decode("utf-8")
+
+def parse_msgs():
+    global parsed_msgs, recv_msgs
+    plot_cmd  = None
+    plot_data = {}
+    while (not kill_thread):
         zmq_message = None
-        if (not msg_queue.empty()):
-            zmq_message = msg_queue.get()
-            msg_queue.task_done()
+        if (not recv_msgs.empty()):
+            zmq_message = recv_msgs.get()
+            recv_msgs.task_done()
         else:
             continue
         
         if (zmq_message[0:4] == b"data"):
-            data_info     = zmq_message.decode("utf-8").split('|')
-            # 0: data, 1: var_name, 2: var_type, 3: n_elems, 4: array_shape
-            data_type     = data_info[TYPE_IDX]
-            data_len      = int(data_info[LEN_IDX])
-            data_shape    = parse_shape(data_info[SHAPE_IDX])
-            data_payload  = msg_queue.get()
-            plot_data[data_info[SYM_IDX]] = handle_payload(data_payload, data_type, data_len, data_shape)
-            msg_queue.task_done()
-        elif(zmq_message[0:8] == b"finalize"):
-            aeval.symtable = {**aeval.symtable, **plot_data}
-            aeval.eval(plot_cmd)
+            header = zmq_message
+            data   = recv_msgs.get()
+            recv_msgs.task_done()
+            plot_data = update_data(header, data, plot_data)
 
-            # Some error happened pause execution by creating sample matplotlib windows
-            if aeval.error_msg != None:
-                aeval.error_msg = None
-                plt.figure(figsize=(6,5))
-                plt.title("Exception from ASTEVAL, check stdout", fontsize=14)
-                plt.show()
-            plot_cmd  = None
+        elif (zmq_message[0:8] == b"finalize"):
+            parsed_msgs.put(("plot", plot_cmd, plot_data, ))
+            plot_cmd = None
             plot_data = {}
-        elif(zmq_message == b"exit"):
-            print("[INFO] Received exit message, exiting")
-            kill_thread = True
-            subscriber_thread.join()
-            sys.exit(0)
+        elif (zmq_message == b"exit"):
+            parsed_msgs.put(("exit", 0,))
         else:
-            # print("[INFO] Executing ... \n")
-            plot_cmd = zmq_message.decode("utf-8")
-            # print(plot_cmd, '\n')
-except KeyboardInterrupt as e:
-    print("[Error] Received keyboardInterrupt, killing subscriber thread ")
+            plot_cmd = update_cmd(zmq_message)
+
+def plot_handler(plot_cmd:str, plot_data:dict)->None:
+    global aeval
+    print("[INFO] plotting ...")
+    aeval.symtable = {**aeval.symtable, **plot_data}
+    aeval.eval(plot_cmd)
+
+    if (aeval.error_msg != None):
+        aeval.error_msg = None
+        plt.figure(figsize=(6,5))
+        plt.title("Exception from ASTEVAL, check stdout", fontsize=14)
+        plt.show()
+    print("[INFO] done")
+
+def exit_handler(exit_code):
+    global kill_thread
     kill_thread = True
-    subscriber_thread.join()
-    sys.exit(e)
+    print("[INFO] requested to shutdown, goodbye!")
+
+def run_main():
+    global parsed_msgs
+    cmd_handler = {}
+    cmd_handler["plot"] = plot_handler
+    cmd_handler["exit"] = exit_handler
+
+    print(f"[INFO] plotting server initialized")
+    try:
+        while(not kill_thread):
+            if (not parsed_msgs.empty()):
+                msg = parsed_msgs.get_nowait()
+                parsed_msgs.task_done()
+                func = cmd_handler[msg[0]]
+                func(*(msg[1:]))
+    except KeyboardInterrupt:
+        print("[Warning] received keyboardInterrupt, killing plotting server")
+        exit_handler(0)
+
+#### main ####
+if __name__ == '__main__':
+    addr = "tcp://127.0.0.1:5555"
+    if (len(sys.argv) > 1):
+        addr = sys.argv[1]
+    
+    sub_thread = Thread(target=subscriber, args=[addr])
+    parser_thread = Thread(target=parse_msgs)
+
+    sub_thread.start()
+    parser_thread.start()
+    
+    run_main()
+
+    sub_thread.join()
+    parser_thread.join()
